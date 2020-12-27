2cbd38c7cec51f29e66be15641f02454bce0b0b7
rkraushan85 <raushan.r@ascentbusiness.com>
2020-12-27 19:23:19 +0530

    Modified SolarEnergyTrade solidity

--- contracts/SolarEnergyTrading.sol
+++ contracts/SolarEnergyTrading.sol
@@ -32,6 +32,25 @@ struct Offer {
         newOffer.L2P_Paid = false;
         newOffer.deliverd = false;
         offerId += 1;
+    }
 
+    function pickOffer(uint32 id) public{
+        offers[id].picked = true;        
+    }
+
+    function confirmP2L_Tx(uint32 id,string P2L_Hash) public{
+        offers[id].P2L_Paid = true;
+        offers[id].P2L_Hash = P2L_HASH;
+        offers[id].buyer = msg.sender;
+        offers[id].timePaidP2L = now;
+    }
+
+    function PoD(uint32 id) public{
+        offers[id].deliverd = true;
+    }
+
+    function confirmL2P_Tx(uint32 id,string L2P_Hash) public{
+        offers[id].L2P_Paid = true;
+        offers[id].L2P_Hash = L2P_HASH;        
     }
 }
